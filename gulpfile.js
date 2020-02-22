203927ed760c16542caa771b43103e64eeeb6a3e
list109 <git_109@list.ru>
2020-02-22 12:43:29 +0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,96 @@
-let gulp      			= require('gulp'),
-sass       				= require('gulp-sass'),
-browserSync 			= require('browser-sync'),
-uglify					= require('gulp-uglify'),
-concat					= require('gulp-concat'),
-rename					= require('gulp-rename'),
-sourcemaps 				= require('gulp-sourcemaps'),
-cssnano 				= require('gulp-cssnano'),
-newer					= require('gulp-newer'),
-cache					= require('gulp-cache'),
-autoprefixer 			= require('gulp-autoprefixer'),
-tinyPng					= require('gulp-tinypng'),
-imagemin				= require('gulp-imagemin'),
-imageminPngquant 		= require('imagemin-pngquant'),
-imageminJpegRecompress 	= require('imagemin-jpeg-recompress'),
-del 					= require('del');//удаление файлов или директорий,
-spritesmith				= require('gulp.spritesmith'), // спрайты
+let gulp = require('gulp'),
+	path = require('path'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	sourcemaps = require('gulp-sourcemaps'),
+	cssnano = require('gulp-cssnano'),
+	newer = require('gulp-newer'),
+	cache = require('gulp-cache'),
+	autoprefixer = require('gulp-autoprefixer'),
+	tinyPng = require('gulp-tinypng'),
+	imagemin = require('gulp-imagemin'),
+	imageminPngquant = require('imagemin-pngquant'),
+	imageminJpegRecompress = require('imagemin-jpeg-recompress'),
+	del = require('del');//удаление файлов или директорий,
+	spritesmith = require('gulp.spritesmith'), // спрайт
 
-// сравнение исходных фалов и файлов сборки
-changed					= require('gulp-changed'),
-// проверка исходных файлов на месте
-changedInPlace 			= require('gulp-changed-in-place'), 
-// изменение размеров картинки
-imageResize				= require('gulp-image-resize'),
-// параллельное преобразование
-parallel				= require('concurrent-transform');
+	svgmin = require('gulp-svgmin'), // минификация и изменение имени svg перед сборкой в спрайт
+	cheerio = require('gulp-cheerio'), // изменение html и xml (используеться для удаления атрибутов style, fill, stroke у svg в данной сборке)
+	svgstore = require('gulp-svgstore'), // svg спрайт
+	inject = require('gulp-inject'), // подключение потоков файлов в различные форматы файлов 
+
+
+
+
+	// сравнение исходных фалов и файлов сборки
+	changed = require('gulp-changed'),
+	// проверка исходных файлов на месте
+	changedInPlace = require('gulp-changed-in-place'),
+	// изменение размеров картинки
+	imageResize = require('gulp-image-resize'),
+	// параллельное преобразование
+	parallel = require('concurrent-transform');
 
 
 
 
 //Преобразование в css и инжектинг при изменении всех sass файлов кроме либов;
-gulp.task('sass', function() {
+gulp.task('sass', function () {
 	return gulp.src(['app/sass/*.+(sass|scss)'])
-	.pipe(sourcemaps.init())
-	.pipe(sass({
-		includePaths: require('scss-resets').includePaths // include normilize in sass file by @import 'normalize';
-	}).on('error', sass.logError))
-	.pipe(rename({
-		suffix: ".min"
-	}))
-	.pipe(sourcemaps.write('./'))
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.stream());
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			includePaths: require('scss-resets').includePaths // include normilize in sass file by @import 'normalize';
+		}).on('error', sass.logError))
+		.pipe(rename({
+			suffix: ".min"
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.stream());
 });
 
 // Преобразование либов в css и инжектинг при изменениях;
 gulp.task('sass-libs', () => {
 	return gulp.src('app/sass/libs/libs.+(scss||sass||css)')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(rename({
-		suffix: ".min",
-	}))
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.stream());
+		.pipe(sass().on('error', sass.logError))
+		.pipe(rename({
+			suffix: ".min",
+		}))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.stream());
 });
 
 
 //Конкатенация и минификация всех js библиотек;
 gulp.task('scripts', () => {
 	return gulp.src(['app/js/libs/*.js'])
-	.pipe(sourcemaps.init())
-	.pipe(concat('libs.min.js'))
-	.pipe(uglify())
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest('app/js'));
+		.pipe(sourcemaps.init())
+		.pipe(concat('libs.min.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('app/js'));
 });
 
 
 //=======================Спрайты===================================================
 
 // Подгонка размеров картинок под спрайты
-gulp.task('imgResize', function() {
-	return gulp.src('app/sprites/originalFiles/*.+(png||jpeg||jpg)')
-	// берем только те файлы, которые отличаються от файлов на выходе
-	.pipe(changed('app/sprites/readyFiles/', { 
-		extension: '.png' // учесть изменение расширения
-	}))
-	.pipe(parallel(
-		imageResize({
-			width: 50,
-			height: 50,
-			format: 'png', // изменить расширение
-		})))
-	.pipe(gulp.dest('app/sprites/readyFiles/'));
+gulp.task('imgResize', function () {
+	return gulp.src('app/img/sprites/originalFiles/*.+(png||jpeg||jpg)')
+		// берем только те файлы, которые отличаються от файлов на выходе
+		.pipe(changed('app/img/sprites/readyFiles/', {
+			extension: '.png' // учесть изменение расширения
+		}))
+		.pipe(parallel(
+			imageResize({
+				width: 50,
+				height: 50,
+				format: 'png', // изменить расширение
+			})))
+		.pipe(gulp.dest('app/img/sprites/readyFiles/'));
 });
 
 //Создание спрайтов и их стилей
@@ -90,25 +99,78 @@ gulp.task('imgResize', function() {
 2). создается json;
 3). с помощью выбранного формата используется нужный шаблон с даными json для соз
 дания файла css, scss или др.*/
-gulp.task('sprite', gulp.series('imgResize', function(done) {
-	let spriteData = gulp.src('app/sprites/readyFiles/*.+(png||jpeg||jpg)')
-	.pipe(spritesmith({
-		imgName: 'sprite.png',
-		imgPath: '../img/sprites/sprite.png',
-		cssName: '_sprite-mixin.scss',
-		cssFormat: 'scss',
-		algorithm: 'binary-tree',
-		cssTemplate: 'app/templates/scss-sprite-template.handlebars', // пользовательский шаблон
-		cssVarMap: function(sprite) {
-			sprite.name = 's-' + sprite.name;
-		},
-	}));
+gulp.task('sprite', function (done) {
+	let spriteData = gulp.src('app/img/sprites/readyFiles/*.+(png||jpeg||jpg)')
+		.pipe(spritesmith({
+			imgName: 'sprite.png',
+			imgPath: '../img/sprites/sprite.png',
+			cssName: '_sprite-mixin.scss',
+			cssFormat: 'scss',
+			algorithm: 'binary-tree',
+			cssTemplate: 'app/templates/scss-sprite-template.handlebars', // пользовательский шаблон
+			cssVarMap: function (sprite) {
+				sprite.name = 's-' + sprite.name;
+			},
+		}));
 
 	spriteData.img.pipe(gulp.dest('app/img/sprites/'));
 	spriteData.css.pipe(gulp.dest('app/sass/myLibs/'));
 
 	done();
-}));
+});
+
+//=======================SVG Спрайты===================================================
+
+gulp.task('svgsprite', function () {
+	var svgs = gulp.src('app/img/sprites/svg/**/*.svg')
+		// если есть файлы во вложенных каталогах, то добавляем названия каталогов в имя файла в начало через разделитель "-", 
+		// чтобы не было конфликтов svg файлов с одинаковым названием при преобразовании в id в спрайте
+		.pipe(rename(function (file) { 
+            var name = file.dirname.split(path.sep);
+			name.push(file.basename);
+			if(name[0] != '.') {
+				file.basename = name.join('-');
+			}
+            
+        }))
+		.pipe(svgmin(function getOptions (file) {
+            var prefix = path.basename(file.relative, path.extname(file.relative));
+            return {
+                plugins: [{
+                    cleanupIDs: {
+                        prefix: prefix + '-',
+                        minify: true
+                    }
+                }]
+            }
+		}))
+		// генерация svg спрайта +
+		// отключение некоторых элементов(<?xml ?> и DOCTYPE) которые не нужны при инлайновом использовании
+		.pipe(svgstore({ inlineSvg: true }))
+		// удаление атрибутов fill и style у svg 
+		.pipe(cheerio({
+            run: function ($) {
+				$('[fill]:not([fill="none"])').removeAttr('fill');
+				$('[style]').removeAttr('style');
+            },
+            parserOptions: { xmlMode: true }
+        }))
+
+		function fileContents (filePath, file) {
+			return file.contents.toString();
+		}
+		// +инлайнового подключения - позволит избежать проблем с градиентами в различных браузерах
+		// -инлайнового подключения - отсутствие кеширования
+		// можно использовать оба метода: пользоваться 2 способом и при необходимости использовать инлайновый если есть градиенты?...
+		// ...для использования способа с внешним файлом нужно подключить дополнительно svg4everybody для поддержки подключения внешних файлов в IE < 11
+		return gulp
+        .src('app/index.html')
+        .pipe(inject(svgs, { transform: fileContents }))
+        .pipe(gulp.dest('app/'));
+});
+
+
+
 //=================================================================================
 
 //Создание сервера browserSync;
@@ -127,9 +189,12 @@ gulp.task('browser-sync', done => {
 gulp.task('watch', (done) => {
 	gulp.watch(['app/sass/*.+(sass|scss)', 'app/sass/myLibs/*.+(sass|scss)'], gulp.series('sass')); //все sass и scss кроме внешних либов;
 	gulp.watch('app/sass/libs/*.+(sass||scss||css)', gulp.series('sass-libs')); // только внешние либы;
-	gulp.watch('app/js/**/*.js', (done) => {browserSync.reload(), done()});
-	gulp.watch(['app/html/**/*.html', 'app/index.html'], (done) => {browserSync.reload(), done()});
-	gulp.watch('app/img/sprites/originalFiles/*.+(png||jpg||jpeg)', gulp.series('sprite'));
+	gulp.watch('app/js/**/*.js', (done) => { browserSync.reload(), done() });
+	gulp.watch(['app/html/**/*.html', 'app/index.html'], (done) => { browserSync.reload(), done() });
+	//c преобразовынием к одинаковым размерам (выставлено 50*50);
+	gulp.watch('app/img/sprites/originalFiles/*.+(png||jpg||jpeg)', gulp.series('imgResize', 'sprite'));
+	//без преобразования к одинаковым размерам (как есть);
+	gulp.watch('app/img/sprites/readyFiles/*.+(png||jpg||jpeg)', gulp.series('sprite'));
 	done();
 });
 
@@ -137,8 +202,8 @@ gulp.task('watch', (done) => {
 запуск таксов перед наблюдением, 
 запуск наблюдения*/
 gulp.task('default', gulp.parallel(
-	'browser-sync', 'sass', 'sass-libs', 'scripts', 'sprite', 'watch'
-	), done => done());
+	'browser-sync', 'sass', 'sass-libs', 'scripts', 'sprite', 'svgsprite', 'watch'
+), done => done());
 
 
 //очистка кеша
@@ -147,8 +212,8 @@ gulp.task('clearCache', () => {
 });
 
 // Ручное удаление ненужных css и js в app
-gulp.task('clean-css', done => {del.sync(['app/css/*.*', '!app/css/*main.min.css', '!app/css/*libs.min.css']); done();}); //не необходимо;
-gulp.task('clean-js', done => {del.sync(['app/js/*.*', '!app/css/*.min.js', '!app/js/main.js']); done();}); //не необходимо;
+gulp.task('clean-css', done => { del.sync(['app/css/*.*', '!app/css/*main.min.css', '!app/css/*libs.min.css']); done(); }); //не необходимо;
+gulp.task('clean-js', done => { del.sync(['app/js/*.*', '!app/css/*.min.js', '!app/js/main.js']); done(); }); //не необходимо;
 
 
 //BUILD/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -162,82 +227,83 @@ gulp.task('clean', (done) => {
 
 
 // 1). основной способ сжатия картинок;
-gulp.task('compress', (done) => { 
+gulp.task('compress', (done) => {
 
 	gulp.src(['app/img/**/*.png', 'app/img/**/*.jpg'])
-	.pipe(cache(tinyPng('ejsl52J9UzLtVC4shtRvKIwajsaTxp0J'),
-		{name:'images-tinyPng(png/jpg)'}))
-	.pipe(gulp.dest('prod/img/'));
+		.pipe(cache(tinyPng('ejsl52J9UzLtVC4shtRvKIwajsaTxp0J'),
+			{ name: 'images-tinyPng(png/jpg)' }))
+		.pipe(gulp.dest('prod/img/'));
 	gulp.src(['app/img/**/*.svg', 'app/img/**/*.gif'])
-	.pipe(cache(imagemin([
-		imagemin.gifsicle({interlaced: true}),
-		imagemin.jpegtran({progressive: true}),
-		imagemin.optipng({optimizationLevel: 5}),
-		imagemin.svgo({
-			plugins: [
-			{removeViewBox: false},
-			{cleanupIDs: false}
-			]
-		})
-		])), {name: 'images-imagemin(svg/gif)'})
-	.pipe(gulp.dest('prod/img'));
+		.pipe(cache(imagemin([
+			imagemin.gifsicle({ interlaced: true }),
+			imagemin.jpegtran({ progressive: true }),
+			imagemin.optipng({ optimizationLevel: 5 }),
+			imagemin.svgo({
+				plugins: [
+					{ removeViewBox: false },
+					{ cleanupIDs: false }
+				]
+			})
+		])), { name: 'images-imagemin(svg/gif)' })
+		.pipe(gulp.dest('prod/img'));
 	done();
 });
 
 
 // 2). альтернативный способ сжатия картинок вместо tinyPng;
-gulp.task('altCompress', () => { 
+gulp.task('altCompress', () => {
 	return gulp.src('app/img/**/*')
-	.pipe(cache(imagemin([
-		imagemin.gifsicle({interlaced: true}),
-		imagemin.jpegtran({progressive: true}),
-		imagemin.optipng({optimizationLevel: 5}),
-		imagemin.svgo({
-			plugins: [
-			{removeViewBox: false},
-			{cleanupIDs: false}
-			]
-		}),
-		imageminJpegRecompress(),
-		imageminPngquant()
-		])), {name: 'images-imagemin(all types)'})
-	.pipe(gulp.dest('prod/img'));
+		.pipe(cache(imagemin([
+			imagemin.gifsicle({ interlaced: true }),
+			imagemin.jpegtran({ progressive: true }),
+			imagemin.optipng({ optimizationLevel: 5 }),
+			imagemin.svgo({
+				plugins: [
+					{ removeViewBox: false },
+					{ cleanupIDs: false }
+				]
+			}),
+			imageminJpegRecompress(),
+			imageminPngquant()
+		])), { name: 'images-imagemin(all types)' })
+		.pipe(gulp.dest('prod/img'));
 });
 
 // В css добавляються префиксы, минификация, карты исходного кода и перенос в prod;
 gulp.task('css', () => {
 	gulp.src('app/css/*.css')
-	.pipe(sourcemaps.init())
-	.pipe(autoprefixer({
-		overrideBrowserslist: ['last 2 versions'],
-		cascade: false
-	}))
-	.pipe(cssnano())
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest('prod/css/'));
+		.pipe(sourcemaps.init())
+		.pipe(autoprefixer({
+			overrideBrowserslist: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(cssnano())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('prod/css/'));
 
 	return gulp.src('app/css/libs.min.css') // либы уже сжатые, их просто перенести;
-	.pipe(gulp.dest('prod/css/'));
+		.pipe(gulp.dest('prod/css/'));
 });
 
 // сам процесс сборки
 gulp.task('build',
 	gulp.series(
 		'clean',
-		'sass', 
-		'sass-libs', 
-		'scripts', 
+		'sass',
+		'sass-libs',
+		'scripts',
 		'compress',
 		'css',
 		(done) => {
 			gulp.src('app/fonts/**/*.*')//fonts;
-			.pipe(gulp.dest('prod/fonts'));
+				.pipe(gulp.dest('prod/fonts'));
 			gulp.src('app/js/**/*')//js;
-			.pipe(gulp.dest('prod/js'));
+				.pipe(gulp.dest('prod/js'));
 			gulp.src('app/*.html')//index html;
-			.pipe(gulp.dest('prod/'));
+				.pipe(gulp.dest('prod/'));
 			gulp.src('app/html/**/*.html')//the rest of html;
-			.pipe(gulp.dest('prod/html'));
-			done();}
-			)
-	);
+				.pipe(gulp.dest('prod/html'));
+			done();
+		}
+	)
+);
