ff1fd3e46ac2a3dd76f90d3ba3dc7c829524b4a5
list109 <git_109@list.ru>
2020-05-07 19:55:05 +0300

    menu
    minification of mobile-menu feature

--- app/js/libs/menu.js
+++ app/js/libs/menu.js
@@ -1,18 +1,6 @@
-function menu(btn, btnClass, {
-    elements,
-    classList,
-}) {
+function menu(btn, btnClass) {
     btn.onclick = () => {
-        if(!btn.classList.contains(btnClass)) {
-            let coords = btn.getBoundingClientRect();
-            btn.style.left = coords.left + 'px';
-            btn.style.top = coords.top + 'px';
-        } else {
-            btn.style.top = btn.style.left = "";
-        }
-        btn.classList.toggle(btnClass);
-        elements.forEach((item, i) => {
-            if(classList[i])item.classList.toggle(classList[i]);
-        });
+            btn.classList.toggle(btnClass);
+            document.body.classList.toggle(btn.dataset.bodyClass);
     }
 };
